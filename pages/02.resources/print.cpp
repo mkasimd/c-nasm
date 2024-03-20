a308a6ee2ed92c36064540ff26fb39996e6d72e1
M. Kasim <49522425+mkasimd@users.noreply.github.com>
2024-03-20 09:07:10 +0100

    Update print.cpp
    * Added 'input' function for reading from STDIN
    * Changed return types to 'int' instead of 'void' to be more standard-conform

--- pages/02.resources/print.cpp
+++ pages/02.resources/print.cpp
@@ -1,32 +1,33 @@
 /*
-* Works only on x86 & x86_64 Linux systems
-* But can be extended to work on other achitectures
-* and OS if you know the OS' ABI as well as
-* the assembly syntax for that architecture
-*
-* Supplies:
-*          * void print(char*)
-*          * void println(char*)
-*          * void printerr(char*)
-*          * long unsigned int strlen(char*)
-*
-* Note: You might want to use 'g++ -Wno-write-strings'
-*
-* Author: github.com/mkasimd
-*/
+ * Works only on x86 & x86_64 Linux systems
+ * But can be extended to work on other achitectures
+ * and OS if you know the OS' ABI as well as
+ * the assembly syntax for that architecture
+ *
+ * Supplies:
+ *          * int print(char*)
+ *          * int println(char*)
+ *          * int printerr(char*)
+ *          * int input(char* dst, int len)
+ *          * long unsigned int strlen(char*)
+ *
+ * Note: You might want to use 'g++ -Wno-write-strings'
+ *
+ * Author: github.com/mkasimd
+ */
 
 // forward declaration of functions
 long unsigned int strlen(char*);
 
 // setting environment specific values and functions
 #ifndef NULL
-#ifdef __cplusplus
+ #define NULL 0
+ #ifdef __cplusplus
   #if __cplusplus > 201103L
+  #undef NULL
   #define NULL nullptr
   #endif
-#else
-  #define NULL 0
-#endif
+ #endif
 #endif // in case NULL wasn't defined yet, you're welcome
 
 #ifdef linux
@@ -36,10 +37,10 @@ long unsigned int strlen(char*);
     #define SYSWRITE 1
   #endif
   #if defined(__i386__)
-    #define SYSREAD 1
+    #define SYSREAD 3
     #define SYSWRITE 4
   #endif
-  #define STDIN  0 
+  #define STDIN 0
   #define STDOUT 1
   #define STDERR 2
 #endif
@@ -58,96 +59,131 @@ long unsigned int strlen(char*);
   #define ARCH 0 // unsupported CPU architecture
 #endif
 
-#define STR_IMPL_(x) #x      //stringify argument
-#define STR(x) STR_IMPL_(x)  //indirection to expand argument macros
+#define STR_IMPL_(x) #x     // stringify argument
+#define STR(x) STR_IMPL_(x) // indirection to expand argument macros
+
 #if ARCH == 64 && OS == 'L'
-  /* Assembly freaks out many people as is,
-   * so let's use the power of macros here
-   * and make the assembly code below much easier to read 
-   */
+/* Assembly freaks out many people as is,
+ * so let's use the power of macros here
+ * and make the assembly code below much easier to read
+ * through macro expansions within the asm block
+ */
   #define WRITEOUT "mov $" STR(SYSWRITE) ", %rax\n\t"
-  void print_64(int FILE, char* msg, int len); // forward declaration
+  #define READ "mov $" STR(SYSREAD) ", %rax\n\t"
+
+   // forward-declare architecture-specific functions
+  int print_64(int FILE, char* msg, int len);
+  int input_64(int FILE, char* dst, int len);
 
   // x86_64 Linux specific print & printerr functions
-  void print(char* str) { return print_64(STDOUT, str, strlen(str)); }
-  void printerr(char* str) { return print_64(STDERR, str, strlen(str)); }
+  int print(char* str) { return print_64(STDOUT, str, strlen(str)); }
+  int printerr(char* str) { return print_64(STDERR, str, strlen(str)); }
+  int input(char* dst, int len) { return input_64(STDIN, dst, len); }
 #endif
 #if ARCH == 86 && OS == 'L'
-  void print_32(int FILE, char* msg, int len); // forward declaration
+  int print_32(int FILE, char* msg, int len); // forward declaration
+  int input_32(int FILE, char* dst, int len);
 
   // x86 Linux specific print & printerr functions
-  void print(char* str) { return print_32(STDOUT, str, strlen(str)); }
-  void printerr(char* str) { return print_32(STDERR, str, strlen(str)); }
+  int print(char* str) { return print_32(STDOUT, str, strlen(str)); }
+  int printerr(char* str) { return print_32(STDERR, str, strlen(str)); }
+  int input(char* dst, int len) { return input_32(STDIN, dst, len); }
 #endif
-#ifndef ARCH 
+#ifndef ARCH
   #define ARCH 0 // unsupported CPU architecture
 #endif
 
-
 #if OS != 'L' || ARCH == 0 // For unsupported OS & architectures only
 
-
-  // Let them repent choosing a bad OS or architecture ;)
-  #include <stdio.h>
-  int main(void) {
-    fprintf(stderr, "This program solely works on x86 & x86_64 Linux systems.\n\
+// Let them repent choosing a bad OS or architecture ;)
+#include <stdio.h>
+int main(void) {
+  fprintf(stderr, "This program solely works on x86 & x86_64 Linux systems.\n\
                     Get yourself a proper computer ;)\n");
-    return -1;
-  }
-
+  return -1;
+}
 
 #else // everything else: For supported OS & architectures only
 
-
 // general values and macro functions
-#define println(STR) {print(STR); print("\n");}
+#define println(STR)                                                           \
+  {                                                                            \
+    print(STR);                                                                \
+    print("\n");                                                               \
+  }
 #define ENDOF(STR) (*STR == '\0')
 
-
-
 /*****************************************************************************
-*                                                                            *
-*                   ACTUAL CODE TO EXECUTE COMES HERE                        *
-*                                                                            *
-*****************************************************************************/
+ *                                                                            *
+ *                   ACTUAL CODE TO EXECUTE COMES HERE                        *
+ *                                                                            *
+ *****************************************************************************/
 
 int main(void) {
   print("Yes, it works!\n");
   println("Macro expansions are awesome, aren't they? ;)");
   printerr("Of course, you can write to STDERR as well ^^\n");
+  print("Try inputting text as well:\n");
+  char buf[50];
+  int x = input(buf, 49);
+  print("You've written: ");
+  println(buf);
 }
 
-
 /*****************************************************************************
-*                                                                            *
-*               SELF-IMPLEMENTED LIBRARY FUNCTIONS BELOW                     *
-*                                                                            *
-*****************************************************************************/
+ *                                                                            *
+ *               SELF-IMPLEMENTED LIBRARY FUNCTIONS BELOW                     *
+ *                                                                            *
+ *****************************************************************************/
 
 long unsigned int strlen(char* str) {
   long unsigned int size = 0;
-  while( !ENDOF(str++) )
+  while (!ENDOF(str++))
     size++;
   return size;
 }
 
 #if defined(__x86_64__)
 // As per x64 GCC calling convention: rdi, rsi, rdx, rcx, r8, r9, stack...
-void print_64(int FILE, char* msg, int len) {
-  __asm__(               // FILE, msg & len in the correct registers already
-    WRITEOUT             // see macro definition above, if this looks too abstract
-    "syscall"            
+int print_64(int FILE, char* msg, int len) {
+  __asm__(      // FILE, msg & len in the correct registers already
+      WRITEOUT  // see macro definition above, if this looks too abstract
+      "syscall" //
   );
+  register int ret asm("rax");
+  return ret;
+}
+
+int input_64(int FILE, char* dst, int len) {
+  asm(          //
+      READ      //
+      "syscall" //
+  );            // return value in RAX
+  register int ret asm("rax");
+  dst[ret] = '\0';
+  return ret;
 }
 #endif
 
 #if defined(__i386__)
-void print_32(int FILE, char* msg, int len) {
-  __asm__(
-    "int $0x80" // system interrupt
-    :
-    :"a"(SYSWRITE), "b"(FILE), "c"(msg), "d"(len)
+int print_32(int FILE, char* msg, int len) {
+  __asm__("int $0x80" // system interrupt
+          :
+          : "a"(SYSWRITE), "b"(FILE), "c"(msg),
+            "d"(len)
   ); // set register values: eax = SYS_WRITE, ebx = FILE, ...
+  register int ret asm("eax");
+  return ret;
+}
+
+int input_32(int FILE, char* dst, int len) {
+  int ret;
+  __asm__("int $0x80" //
+          : "=r"(ret) //
+          : "a"(SYSREAD), "b"(FILE), "c"(dst), "d"(len)
+  );
+  dst[ret] = '\0'; // NULL-terminate input string
+  return ret;
 }
 #endif
 #endif // end of "For supported OS & architectures only"
